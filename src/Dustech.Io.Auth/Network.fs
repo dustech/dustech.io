13fdb7a9954488244e8b7a61b2983ddb1b5cf995
dustech <stefano.cerruti@outlook.com>
2025-03-24 15:16:21 +0100

    Add auth project

---
+++ src/Dustech.Io.Auth/Network.fs
@@ -0,0 +1,10 @@
+﻿namespace TheGrindOrDie.Auth.Config
+
+[<AutoOpen>]
+module Network =
+    type NetworkConfiguration =
+        { Proxied: bool
+          AuthorityInternalUri: string
+          AuthorityExternalUri: string
+          WebBffInternalUri: string
+          WebBffExternalUri: string }
